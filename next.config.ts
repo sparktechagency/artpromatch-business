07a20051dc729bcced450f24bd1d6dc6e2706bd6
Khaled Siddique <khaledssbd@gmail.com>
2025-12-25 12:24:14 +0600

    some updates done

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'example.com',
       },
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
     ],
   },
   experimental: {
